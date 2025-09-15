ce316b8ccbb593bd2ad7ea7d90da225934d580f3
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-09-15 22:50:52 +0530

    Update Navbar.jsx

--- my-TimeCapsule/src/component/Navbar.jsx
+++ my-TimeCapsule/src/component/Navbar.jsx
@@ -4,15 +4,14 @@ function Navbar() {
   return (
     <nav className="p-4 bg-gray-100 shadow">
       <div className="flex gap-6 justify-center">
-        <Link to="/my" className="text-blue-600 hover:underline">
-          My Capsule
-        </Link>
+       
         <Link to="/create" className="text-blue-600 hover:underline">
           Create a Capsule
         </Link>
-        <Link to="/all" className="text-blue-600 hover:underline">
-          All Capsules
+        <Link to="/unlock" className="text-blue-600 hover:underline">
+          Unlock Capsule
         </Link>
+
       </div>
     </nav>
   );
